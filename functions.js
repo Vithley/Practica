847c400f1dc4e8d364e7acad85a8028a8aa5e151
Carolina <carolina.millan@cursodesfufor.onmicrosoft.com>
2022-11-24 13:44:01 +0100

    cambio a más archivos

---
+++ functions.js
@@ -0,0 +1,162 @@
+import {
+    availableFemaleNames,
+    availableMaleNames,
+    availableGenders, 
+    students
+} from "./chanlleged.js"
+
+// 1.Mostrar todos los alumnos en una tabla
+const showTables = () => {
+    console.table(students);
+  }
+  
+// 2.Mostrar el total de alumnos que hay en la clase
+const numberOfStudents= () => {
+let numStudents = students.length;
+console.log(`El número de alumnos que hay en la clase es ${numStudents}.`)
+}
+
+// 3.Mostrar todos los nombres de los alumnos
+const nameOfStudents = () => {
+let nameStudents = students.map(function(students) {
+    return students.name;
+});
+console.table(nameStudents);
+
+}
+
+// 4.Eliminar el último alumno de la clase
+const deleteLastStudent = () => {
+console.table(students.pop());
+console.log(`Has eliminado al último usuario de la lista`);
+}
+
+// 5.Eliminar un alumno aleatorio
+const ramdomStudent = () => {
+while(students.length) {
+    const random = Math.floor(Math.random() * students.length);
+    const el = students.splice(random, 1)[0];
+    console.log(el);
+}
+
+}
+
+// 6.Mostrar todos los alumnos que son chicas
+const femaleStudents = () => {
+let femaleStudent = students.filter(function(el) {
+    return(el.gender === 'female')
+});
+console.log(femaleStudent);
+}
+
+// 7.Mostrar por consola el número de chicos y chicas que hay en la clase.
+const numGenderStudents = () => {
+const females = students.filter(person => person.gender === 'female');
+const males = students.filter(person => person.gender === 'male');
+let totalFemales = females.length;
+let totalMale = males.length;
+console.log(`El numero de chicos es ${totalMale}\nEl número total de chicas es ${totalFemales}`);
+
+
+}
+
+// 8. Mostrar true o false por consola si todos los alumnos de la clase son chicas.
+const femaleStudentsTrue = () => {
+const allFemales = students.every(student => student.gender === 'female');
+console.log(allFemales);
+
+}
+// 9. Mostrar los alumnos que que tengan entre 20 y 25 años
+const ageRangeStudents = () => {
+let rangeStudent = students.filter(function(el) {
+    return(el.age >= 20 && el.age <= 25)
+});
+console.table(rangeStudent);
+}
+
+function numeroAleatorio(min, max) {
+return Math.round(Math.random() * (max - min) + min);
+}
+// 10.Añadir un alumno nuevo con datos
+const addDatas = () => {
+let randomName;
+let randomGender = availableGenders[Math.floor(Math.random() * availableGenders.length)];
+if(randomGender === 'female'){
+    randomName = availableFemaleNames[Math.floor(Math.random() * availableFemaleNames.length)];
+}else {
+    randomName = availableMaleNames[Math.floor(Math.random() * availableMaleNames.length)];
+}
+let randomAge = numeroAleatorio(20,50);
+let newItem = {
+            "age": randomAge, 
+            "gender": randomGender,
+            "examScores": [],
+            "name": randomName 
+            };
+students.push(newItem);
+console.log(newItem);
+
+    
+}
+
+// 11. Mostrar el nombre de la persona más joven.
+const youngestStudent = () => {
+const lowest = students.reduce((previous, current) => {
+    return current.age < previous.age ? current : previous;
+});
+
+console.log('El estudante mas jovén es: ', lowest);
+}
+
+// 12. Mostrar la edad media de los alumnos
+const averageAge = () => {
+const average = students.reduce((total, next) => total + next.age, 0) / students.length;
+console.log('La edad media de los estudiantes es:', Math.round(average));
+}
+
+// 13. Mostrar la edad media de todas las chicas de la clase
+const averageAgeGirls = () => {
+const females = students.filter(girls => girls.gender === 'female');
+const average = females.reduce((total, next) => total + next.age, 0) / females.length;
+
+console.log('La edad media de las chicas de la clase es:', Math.round(average));
+}
+
+// 14. Añadir nueva nota a los alumnos
+const addNotes = (student) => {
+    for(let i = 0; i < student.length; i++) {
+        student[i].examScores.push(Math.floor(Math.random() * 11));
+    }
+    return student;
+}
+
+// 15. Ordenar los alumnos alfabéticamente
+const sortNames = () => {
+const names = students.sort(function(a, b){
+    let nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase();
+    if (nameA < nameB) //sort string ascending
+    return -1;
+    if (nameA > nameB)
+    return 1;
+    return 0; //default return value (no sorting)
+    });
+    console.log(names);
+}
+
+export {
+    showTables,
+    numberOfStudents,
+    nameOfStudents,
+    deleteLastStudent,
+    ramdomStudent,
+    femaleStudents,
+    numGenderStudents,
+    femaleStudentsTrue,
+    ageRangeStudents,
+    addDatas,
+    youngestStudent, 
+    averageAge,
+    averageAgeGirls,
+    addNotes, 
+    sortNames
+};
---
+++ main.js
@@ -0,0 +1,156 @@
+// Configuramos la utilidad de node para que los datos se pidan y se muestren por consola.
+import { clear } from 'console';
+import readline from 'readline';
+import {
+    showTables,
+    numberOfStudents,
+    nameOfStudents,
+    deleteLastStudent,
+    ramdomStudent,
+    femaleStudents,
+    numGenderStudents,
+    femaleStudentsTrue,
+    ageRangeStudents,
+    addDatas,
+    youngestStudent, 
+    averageAge,
+    averageAgeGirls,
+    addNotes, 
+    sortNames
+} from "./functions.js";
+
+
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  //Convertir el dato que metemos por consola a un entero
+   const isInt= (str) => {
+    const integer = parseInt(str);
+    if (Number.isNaN(integer)) {
+        return false; 
+    } else {
+        return true;
+    }
+  }
+  
+  //Pedir un número por consola y comprobar que sea un número
+  function getNumberFromConsole() {
+    const promise = new Promise((resolve, reject) => { 
+            rl.question('Introduce el número: ', (num) => {
+                rl.pause();
+                if (isInt(num)) {
+                    num = Number.parseInt(num);
+                    resolve(num);
+  
+                    
+                } else {
+                    reject('Has de introducir un número');
+                }
+            });
+    });
+    
+    return promise;
+  }
+  
+  
+  
+  
+  //Mostrar menu y elegir una opción
+  function showPossibleOptions() {
+    console.log('1- Mostrar en formato de tabla todos los alumnos.');
+    console.log('2- Mostrar por consola la cantidad de alumnos que hay en clase.');
+    console.log('3- Mostrar por consola todos los nombres de los alumnos.');
+    console.log('4- Eliminar el último alumno de la clase.');
+    console.log('5- Eliminar un alumno aleatoriamente de la clase.');
+    console.log('6- Mostrar por consola todos los datos de los alumnos que son chicas.');
+    console.log('7- Mostrar por consola el número de chicos y chicas que hay en la clase.');
+    console.log('8- Mostrar true o false por consola si todos los alumnos de la clase son chicas.');
+    console.log('9- Mostrar por consola los nombres de los alumnos que tengan entre 20 y 25 años.');
+    console.log('10- Añadir un alumno nuevo con los siguientes datos:\n - Nombre aleatorio\n - Edad aleatoria entre 20 y 50 años\n - Género aleatorio\n - Listado de calificaciones vacío.');
+    console.log('11- Mostrar por consola el nombre de la persona más joven de la clase.');
+    console.log('12- Mostrar por consola la edad media de todos los alumnos de la clase.');
+    console.log('13- Mostrar por consola la edad media de las chicas de la clase.');
+    console.log('14- Añadir nueva nota a los alumnos. Por cada alumno de la clase, tendremos que calcular una nota de forma aleatoria(número entre 0 y 10) y añadirla a su listado de notas.');
+    console.log('15- Ordenar el array de alumnos alfabéticamente según su nombre.');
+  }
+  
+  async function showOptions() {
+     
+    let numberFromConsole;
+    let menu; 
+    do {
+       showPossibleOptions();
+      try {
+          numberFromConsole = await getNumberFromConsole();
+        
+      } catch (error) {
+          console.log(error)
+          process.exit(0)
+      }
+  
+      if(numberFromConsole === 0) {
+        console.log('Número incorrecto! Debe de ser un número entre 1 y 15')
+        rl.close()
+  
+      }
+  
+      switch(numberFromConsole) {
+        case 1:
+          showTables();
+          break;
+        case 2:
+          numberOfStudents();
+          break;
+        case 3:
+          nameOfStudents();
+          break;
+        case 4:
+          deleteLastStudent();
+          break;
+        case 5:
+          ramdomStudent();
+          break;
+        case 6:
+          femaleStudents();
+          break;
+        case 7:
+          numGenderStudents();
+          break;
+        case 8:
+          femaleStudentsTrue();
+          break;
+        case 9:
+          ageRangeStudents();
+          break;
+        case 10:
+          addDatas();
+          break;
+        case 11:
+          youngestStudent();
+          break;
+        case 12:
+          averageAge();
+          break;
+        case 13:
+          averageAgeGirls();
+          break;
+        case 14:
+          addNotes();
+          break;
+        case 15:
+          sortNames();
+          break;
+        default:
+          console.log('Número no válido')
+          rl.close();
+          break;
+      }
+      
+    }  while (numberFromConsole <= 15)
+  }
+  
+  showOptions();
+
